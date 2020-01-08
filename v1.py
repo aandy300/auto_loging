3606fc4ebfd01839765f31b245b711e30bfa5581
aandy300 <aandy300@gmail.com>
2020-01-09 04:52:18 +0800

    v1-2 done

--- v1.py
+++ v1.py
@@ -1,21 +1,48 @@
-import time
-import keyboard
+import time ,sys
+import pyautogui as ms
+from pynput import keyboard
 
-#待解決
-#鍵盤監測不重複 單次就好
+x, y = int, int
+count = 0
 
-def candycorn4s():
-    
-    print('i am candy4s runing...')
+def on_press(key):
+    if key == keyboard.Key.f1: # 監測滑鼠位置 並 儲存
+        global x, y
+        x, y = ms.position()
+        print('鼠標現在位置','X:' ,x ,'Y:' ,y )
+    elif key == keyboard.Key.f2: # 印出儲存的監測位置
+        print(x, y)
+    elif key == keyboard.Key.f3: # 點擊指定位置
+        click_postion()
+    elif key == keyboard.Key.f4: # 連點儲存的位置
+        click_savepostion()
+    elif key == keyboard.Key.f12: # 中止
+        return False
+
+def click_postion():
     while True:
-        if keyboard.is_pressed('F2'):
-            print('定點完成,返回上層')
-            candycorn4s()
-            
-        elif keyboard.is_pressed('F3'):
-            print('run mouse check 4s')
-        elif keyboard.is_pressed('F4'):
-            candycorn4s()
-            print('stop')
+        global x
+        x += 1
+        ms.click(400, 400, clicks=2,button='left', interval=0.1)
+        print('clicked at (400, 400)')
+        print(x, '次')
+        time.sleep(5)
 
-candycorn4s()
+def click_savepostion():
+    while True:
+        global x, y, count
+        count += 1
+        ms.click(x, y, clicks=2,button='left', interval=0.1)
+        print('clicked at (',x ,y ,')')
+        print('已點擊', count, '次')
+        time.sleep(5)
+
+def main():
+    with keyboard.Listener(on_press=on_press) as listener:
+        for _ in range(50):
+            print('still running...')
+            time.sleep(1)
+            if not listener.running:
+                break
+
+main()
