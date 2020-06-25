6e13575687f4a93b2ae5a9dc643d7b159924604e
aandy300 <aandy300@gmail.com>
2020-06-26 01:22:45 +0800

    2020.06.25 add treb mouse

--- chick_mouse_postion.py
+++ chick_mouse_postion.py
@@ -1,6 +1,8 @@
 import time ,sys
-import pyautogui as ms
+import pyautogui as ms #https://pyautogui.readthedocs.io/en/latest/mouse.html
 from pynput import keyboard
+
+
 
 x, y = int, int
 mspoint = int,int
@@ -14,6 +16,8 @@ def on_press(key):
         click_postion()
     elif key == keyboard.Key.f5: # 連點
         ms.click()
+    elif key == keyboard.Key.f4: # 連點
+        terb_chick()
     elif key == keyboard.Key.f1: # 監測位置 #會BUG進 迴圈出不來
         global x, y
         global mspoint
@@ -25,6 +29,20 @@ def on_press(key):
     elif key == keyboard.Key.f2: #儲存監測位置
         x, y = ms.position()
         print(x, y)
+def terb_chick():
+    global x,y
+
+    # exsample:
+    # >>> pyautogui.mouseDown(); pyautogui.mouseUp()  # does the same thing as a left-button mouse click
+    # >>> pyautogui.mouseDown(button='right')  # press the right button down
+    # >>> pyautogui.mouseUp(button='right', x=100, y=200)  # move the mouse to 100, 200, then release the right button up.
+    while True:
+        ms.mouseDown(button='left',x=x,y=y)
+        time.sleep(9)
+        ms.mouseUp()
+        time.sleep(5)
+        
+
 
 def click_postion():
     z = 0
@@ -42,7 +60,7 @@ def click_postion():
 
 def main():
     with keyboard.Listener(on_press=on_press) as listener:
-        for _ in range(1150):
+        for _ in range(1150000000):
             # print('still running...')
             time.sleep(1)
             if not listener.running:
